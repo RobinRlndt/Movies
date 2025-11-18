10ab6e9444cae6d4579cd5c636539b32f50b6c7f
Robin Roelandt <Robin.Roelandt@iMacC.local>
2025-11-18 10:27:06 +0100

    first commit

---
+++ Movies/Movies/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by Robin Roelandt on 18/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
