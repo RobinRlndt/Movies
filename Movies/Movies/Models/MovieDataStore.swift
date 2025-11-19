03a714179493627f85fcc4ca5017ea2b55dcfe0d
Robin <robin.roelandt@student.vives.be>
2025-11-19 21:00:56 +0100

    Update MovieDataStore.swift

--- Movies/Movies/Models/MovieDataStore.swift
+++ Movies/Movies/Models/MovieDataStore.swift
@@ -12,29 +12,48 @@ class MovieDataStore {
      var movies = [Movie]
     
     init(){
-        movies = []
+        movies = [Movie]()
     }
     
     func getMovies() -> [Movie] {
-        let allMovies = movies.map
-        }
-        
+        return movies 
     }
     
-    func getMovies(actor: Actor) -> [Movie] {
-        
+   func getMovies(actor: Actor) -> [Movie] {
+    let filteredMovies = movies.filter { movie in
+        return movie.actors.contains(actor)
     }
+    return filteredMovies
+}
+
     
     func getMovies(director: Director) -> [Movie] {
-        
+    let filteredMovies = movies.filter { movie in
+        return movie.director == director
+    }
+    return filteredMovies
+}
+
+    
+   func getACtors(director: Director) -> [Actor] {
+    let filteredMovies = movies.filter { movie in
+        return movie.director == director
     }
     
-    func getACtors(director: Director) -> [Actor] {
-        
+    let allActors = filteredMovies.flatMap { movie in
+        movie.actors
     }
+    
+    let uniqueActors = Array(Set(allActors))
+    
+    return uniqueActors
+}
+
     
     private func sort() {
-       
+       movies.sort { m1, m2 in
+          m1.releaseDate < m2.releaseDate
+     }
     }
     
     func loadData() async {
@@ -43,14 +62,18 @@ class MovieDataStore {
             print("⏳ Simulating 2-second load delay...")
             try await Task.sleep(for: .seconds(2)) // Simulate long load
             // load movies
-            movies = load("movies.json")
+            let data: [MovieJson] = try load("movies.json")
+             movies = data.map({ movieJson in
+               movieJson.toMovie()
+               })
             // sort
-            
+             sort()
             print("✅ Data loaded successfully.")
             
         } catch {
             print("❌ Failed to load uurrooster:", error)
             //movies is lege array
+             movies = [Movie]()
         }
     }
 
